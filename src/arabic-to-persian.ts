7731c4cb67c1df087f3ecea8e6a4488f7069755d
Tooraj Khatibi <2rajpx@gmail.com>
2018-04-19 01:24:11 +0430

    Add api

--- src/arabic-to-persian.ts
+++ src/arabic-to-persian.ts
@@ -1,4 +1,21 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-export default class DummyClass {}
+
+import * as Helpers from './helpers'
+
+export const UNICODE_MAP = Helpers.UNICODE_MAP
+
+export const unicodeOf = Helpers.unicodeOf
+
+export function convert(text: string, options: Helpers.Options = {}) {
+  const { unicodeMap = Helpers.UNICODE_MAP, defaultValue } = options
+  return Helpers.postConvert(
+    Helpers.replaceByUnicodeMap(
+      Helpers.preConvert(text, options),
+      unicodeMap,
+      defaultValue
+    ),
+    options
+  )
+}
