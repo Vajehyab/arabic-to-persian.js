76ca6159e0512cb9d878d50f8b4b6375f48635b4
Tooraj Khatibi <2rajpx@gmail.com>
2018-04-19 19:10:56 +0430

    refactor: rename and some movements

--- src/arabic-to-persian.ts
+++ src/arabic-to-persian.ts
@@ -2,20 +2,100 @@
 // import "core-js/fn/array.find"
 // ...
 
-import * as Helpers from './helpers'
+import CHAR_MAP from './char-map'
+import PERSIAN_NUMBERS_MAP from './persian-numbers-map'
+import UNICODE_MAP from './unicode-map'
 
-export const UNICODE_MAP = Helpers.UNICODE_MAP
+export type Map = { [key: string]: string }
 
-export const unicodeOf = Helpers.unicodeOf
+export interface Options {
+  charMap?: Map
 
-export function convert(text: string, options: Helpers.Options = {}) {
-  const { unicodeMap = Helpers.UNICODE_MAP, defaultValue } = options
-  return Helpers.postConvert(
-    Helpers.replaceByUnicodeMap(
-      Helpers.preConvert(text, options),
-      unicodeMap,
-      defaultValue
-    ),
-    options
-  )
+  defaultValue?: string
+
+  /**
+   * @default false
+   */
+  persianNumbers?: boolean
+
+  /**
+   * @default true
+   */
+  singleSpace?: boolean
+
+  /**
+   * @default true
+   */
+  trim?: boolean
+
+  unicodeMap?: Map
+}
+
+export {
+  CHAR_MAP,
+  PERSIAN_NUMBERS_MAP,
+  UNICODE_MAP,
+}
+
+export function unicodeOf(c: string) {
+  let result = c
+    .charCodeAt(0)
+    .toString(16)
+    .toUpperCase()
+  if (result.length === 3) {
+    result = `0${result}`
+  }
+  return result
+}
+
+export function replaceByCharMap(text: string, map: Map) {
+  let result = text
+  const replace = Object.keys(map)
+  const by = Object.values(map)
+  for (let i = 0; i < replace.length; i++) {
+    result = result.replace(new RegExp(replace[i], 'g'), by[i])
+  }
+  return result
+}
+
+export function replaceByUnicodeMap(text: string, map: Map, defaultValue?: string) {
+  return text
+    .split('')
+    .map(c => map[unicodeOf(c)] || defaultValue || c)
+    .join('')
+}
+
+export function replaceMultiSpacesWithSingleSpace(text: string) {
+  return text.replace(/ +(?= )/g, '')
+}
+
+export function convert(text: string, options: Options = {}) {
+  const {
+    charMap = CHAR_MAP,
+    defaultValue,
+    persianNumbers = false,
+    singleSpace = true,
+    trim = true,
+    unicodeMap = UNICODE_MAP,
+  } = options
+
+  let result = text
+
+  if (trim) {
+    result = result.trim()
+  }
+
+  if (singleSpace) {
+    result = replaceMultiSpacesWithSingleSpace(result)
+  }
+
+  result = replaceByUnicodeMap(result, unicodeMap, defaultValue)
+
+  result = replaceByCharMap(result, charMap)
+
+  if (persianNumbers) {
+    result = replaceByCharMap(result, PERSIAN_NUMBERS_MAP)
+  }
+
+  return result
 }
---
+++ test/arabic-to-persian.test.ts
@@ -0,0 +1,79 @@
+import * as Api from '../src/arabic-to-persian'
+
+describe('Dummy test', () => {
+  it('works if true is truthy', () => {
+    expect(true).toBeTruthy()
+  })
+})
+
+describe('unicodeOf test', () => {
+  it('returns unicode of character', () => {
+    expect(Api.unicodeOf('٠')).not.toBe('660')
+    expect(Api.unicodeOf('٠')).toBe('0660')
+  })
+  it('returns unicode by 4 characters', () => {
+    expect(Api.unicodeOf('٠')).not.toBe('660')
+    expect(Api.unicodeOf('٠')).toBe('0660')
+  })
+})
+
+describe('replaceByCharMap test', () => {
+  it('converts text', () => {
+    expect(Api.replaceByCharMap('0123456789ABC', Api.PERSIAN_NUMBERS_MAP)).toBe('۰۱۲۳۴۵۶۷۸۹ABC')
+  })
+  it('converts specific chars with custom map', () => {
+    expect(Api.replaceByCharMap('﷼', { '﷼': 'ریال' })).toBe('ریال')
+    expect(Api.replaceByCharMap('۵۰ ٪', { '٪': '%' })).toBe('۵۰ %')
+  })
+})
+
+describe('replaceByUnicodeMap test', () => {
+  it('converts text without defaultValue', () => {
+    expect(Api.replaceByUnicodeMap('ﻭاﮊﻩ', Api.UNICODE_MAP)).not.toBe('ﻭاﮊﻩ')
+    expect(Api.replaceByUnicodeMap('ﻭاﮊﻩ', Api.UNICODE_MAP)).toBe('واژه')
+  })
+  it('converts text with defaultValue', () => {
+    expect(Api.replaceByUnicodeMap('﷼', Api.UNICODE_MAP, '***')).not.toBe('ریال')
+    expect(Api.replaceByUnicodeMap('﷼', Api.UNICODE_MAP, '***')).toBe('***')
+  })
+  it("converts text but doesn't convert chars with undefined unicode", () => {
+    expect(Api.replaceByUnicodeMap('۵۰ ٪', Api.UNICODE_MAP)).not.toBe('50 %')
+    expect(Api.replaceByUnicodeMap('۵۰ ٪', Api.UNICODE_MAP)).toBe('50 ٪')
+  })
+  it('converts chars which are used as words in fact', () => {
+    expect(Api.replaceByUnicodeMap('﷼', { FDFC: 'ریال' })).toBe('ریال')
+  })
+})
+
+describe('replaceMultiSpacesWithSingleSpace test', () => {
+  it('returns text with single space between words', () => {
+    expect(Api.replaceMultiSpacesWithSingleSpace('Hello     world  !  !!   !')).toBe('Hello world ! !! !')
+  })
+})
+
+describe('convert test', () => {
+  it('works with "charMap" option', () => {
+    expect(Api.convert('﷼', { charMap: {} })).toBe('﷼')
+    expect(Api.convert('﷼', { charMap: { '﷼': 'ریال' } })).toBe('ریال')
+  })
+  it('works with "defaultValue" option', () => {
+    expect(Api.convert('﷼')).toBe('﷼')
+    expect(Api.convert('﷼', { defaultValue: "***" })).toBe('***')
+  })
+  it('works with "persianNumbers" option', () => {
+    expect(Api.convert('se7en', { persianNumbers: true })).toBe('se۷en')
+    expect(Api.convert('se7en', { persianNumbers: false })).toBe('se7en')
+  })
+  it('works with "singleSpace" option', () => {
+    expect(Api.convert('Hello  World', { singleSpace: true })).toBe('Hello World')
+    expect(Api.convert('Hello  World', { singleSpace: false })).toBe('Hello  World')
+  })
+  it('works with "trim" option', () => {
+    expect(Api.convert(' Hi ', { trim: true })).toBe('Hi')
+    expect(Api.convert(' Hi ', { trim: false })).toBe(' Hi ')
+  })
+  it('works with "unicodeMap" option', () => {
+    expect(Api.convert('﷼', { unicodeMap: {} })).toBe('﷼')
+    expect(Api.convert('﷼', { unicodeMap: { FDFC: 'ریال' } })).toBe('ریال')
+  })
+})
